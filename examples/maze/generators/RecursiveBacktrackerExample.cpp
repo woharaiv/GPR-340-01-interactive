4550a648e16ac2876c1552feeb284230593bf58e
willow.ohara <willow.ohara@mymail.champlain.edu>
2024-09-23 11:20:50 -0400

    Began maze generation Currently (incorrectly) assumes that 0,0 is the top left; it's really the center

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -4,12 +4,52 @@
 #include <climits>
 bool RecursiveBacktrackerExample::Step(World* w) {
   // todo: implement this
+  //If stack is empty, start at top left
+  //While the stack is not empty:
+    if(!stack.empty()) {
+      Point2D p = stack.back();
+      if(visited[p.y][p.x]) {
+        w->SetNodeColor(p, Color::Black);
+        stack.pop_back();
+        return false;
+      }
+      //Mark the top cell as visited;
+      w->SetNodeColor(p, Color::DarkRed);
+      visited[p.y][p.x] = true;
+      //List visitable neighbors;
+      getVisitables(w, p);
+      switch(visitables.size()) {
+        //If there are no visitables, backtrack
+        case 0:
+          return false;
+        //If the stack has visitable neighbors, choose one of them to add to the stack
+        //If there's only one direction, choose that one, otherwise choose a direction at random
+        case 1:
+          stack.push_back(visitables.back());
+          break;
+        default:
+          stack.push_back(visitables.at(w->Random() % visitables.size()));
+          break;
+      }
+      //Remove the wall between the cell and the neighbor
+      Point2D p2 = stack.back();
+      if(p2.y < p.y)
+        w->SetNorth(p,false);
+      if(p2.x > p.x)
+        w->SetEast(p,false);
+      if(p2.y > p.y)
+        w->SetSouth(p,false);
+      if(p2.x < p.x)
+        w->SetWest(p,false);
+    }
   return false;
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
   visited.clear();
   stack.clear();
+  visitables.clear();
+  stack.push_back({0,0});
   auto sideOver2 = world->GetSize() / 2;
 
   for (int i = -sideOver2; i <= sideOver2; i++) {
@@ -30,10 +70,18 @@ Point2D RecursiveBacktrackerExample::randomStartPoint(World* world) {
 }
 
 std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const Point2D& p) {
-  auto sideOver2 = w->GetSize() / 2;
-  std::vector<Point2D> visitables;
-
-  // todo: implement this
-
+  visitables.clear();
+  //Up
+  if(!visited[p.y-1][p.x])
+    visitables.push_back({p.y-1, p.x});
+  //Right
+  if(!visited[p.y][p.x+1])
+    visitables.push_back({p.y, p.x+1});
+  //Down
+  if(!visited[p.y+1][p.x])
+    visitables.push_back({p.y+1, p.x});
+  //Left
+  if(!visited[p.y][p.x-1])
+    visitables.push_back({p.y, p.x-1});
   return visitables;
 }
--- examples/maze/generators/RecursiveBacktrackerExample.h
+++ examples/maze/generators/RecursiveBacktrackerExample.h
@@ -12,7 +12,7 @@ private:
   std::map<int, std::map<int, bool>> visited;  // naive. not optimal
   Point2D randomStartPoint(World* world);
   std::vector<Point2D> getVisitables(World* w, const Point2D& p);
-
+  std::vector<Point2D> visitables;
 public:
   RecursiveBacktrackerExample() = default;
   std::string GetName() override { return "Recursive Back-Tracker"; };
